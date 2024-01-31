613e043d3476b27721d9537ff50f11428d2e8285
Sujit Sarkar <snigdho532@gmail.com>
2024-01-31 11:34:29 +0600

    Cache video player integrated

--- lib/src/features/home/widget/video_widget.dart
+++ lib/src/features/home/widget/video_widget.dart
@@ -1,9 +1,9 @@
+import 'package:cached_video_player/cached_video_player.dart';
 import 'package:flutter/Material.dart';
 import 'package:order_online_app/core/constants/app_color.dart';
 import 'package:order_online_app/core/widgets/loading_widget.dart';
 import 'package:order_online_app/src/features/home/provider/home_provider.dart';
 import 'package:provider/provider.dart';
-import 'package:video_player/video_player.dart';
 
 class VideoWidget extends StatefulWidget {
   const VideoWidget({super.key, required this.videoUrl});
@@ -18,7 +18,9 @@ class _VideoWidgetState extends State<VideoWidget> {
   @override
   void initState() {
     final HomeProvider homeProvider = Provider.of(context,listen: false);
-    homeProvider.initVideo();
+    WidgetsBinding.instance.addPostFrameCallback((timeStamp) {
+      homeProvider.initVideo();
+    });
     super.initState();
   }
 
@@ -28,7 +30,7 @@ class _VideoWidgetState extends State<VideoWidget> {
     return AspectRatio(
       aspectRatio: homeProvider.videoController.value.aspectRatio,
       child: !homeProvider.loadingVideo && homeProvider.videoController.value.isInitialized
-          ? VideoPlayer(homeProvider.videoController)
+          ? CachedVideoPlayer(homeProvider.videoController)
           : const Center(
               child: LoadingWidget(color: AppColor.primaryColor),
             ),
